498af6e9ab69277a6a733064170c93fefe985dd1
Juan David Mayorga <130000511+JuandaMT@users.noreply.github.com>
2025-11-06 11:58:57 +0100

    ✨ feat: implement redirect to winning or loosing page

--- index.php
+++ index.php
@@ -38,14 +38,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['letra'])) {
 
 // Comprobar si se ha ganado o perdido
 if ($_SESSION['letras_acertadas'] == $_SESSION['palabra']) {
-    echo "¡Enhorabuena! Has ganado :) La palabra era: " . $_SESSION['palabra'] . "<br>";
-    session_destroy();
-    echo '<a href="">Jugar de nuevo</a>';
+    header('Location: ganado.php');
     exit();
 } elseif ($_SESSION['vidas'] <= 0) {
-    echo "Lo siento, has perdido :( La palabra era: " . $_SESSION['palabra'] . "<br>";
-    session_destroy();
-    echo '<a href="">Jugar de nuevo</a>';
+    header('Location: perdido.php');
     exit();
 }
 ?>
