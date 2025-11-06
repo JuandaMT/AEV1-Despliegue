90ab494fec4d820c9a59946b31caffdcf9911cf9
Juan David Mayorga <130000511+JuandaMT@users.noreply.github.com>
2025-11-06 11:52:49 +0100

    ✨ feat: added some new words

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Lista de palabras para el juego
-$palabras = ['elefante', 'jirafa', 'hipopotamo', 'rinoceronte', 'cocodrilo', 'camello', 'chimpance'];
+$palabras = ['elefante', 'jirafa', 'hipopotamo', 'rinoceronte', 'cocodrilo', 'camello', 'chimpance', 'jaguar', 'leon', 'tigre', 'picozapato'];
 
 // Inicializar el juego
 if (!isset($_SESSION['palabra'])) {
